8e3bdf55b71a4db9725c1e653afa7347474fbedf
Ashik-Faysal <ashikfaysal.rc@gmail.com>
2023-02-14 06:35:33 +0600

    added

---
+++ js/script.js
@@ -0,0 +1,59 @@
+document.getElementById("text-bold").addEventListener("click", function(){
+    
+    const textField = document.getElementById("text-field");
+    textField.style.fontWeight = "bold";
+    textField.style.fontSize = "1.5rem";
+});
+
+document.getElementById("text-italic").addEventListener("click", function(){
+    const textField = document.getElementById("text-field");
+    textField.style.fontStyle="italic";
+   
+});
+document.getElementById("text-underLine").addEventListener("click", function(){
+    const textField = document.getElementById("text-field");
+    textField.style.textDecoration = "underline";
+   
+});
+document.getElementById("left-align").addEventListener("click", function(){
+    const textField = document.getElementById("text-field");
+    textField.style.textAlign = "left";
+   
+});
+document.getElementById("center-align").addEventListener("click", function(){
+    const textField = document.getElementById("text-field");
+    textField.style.textAlign = "center";
+   
+});
+document.getElementById("right-align").addEventListener("click", function(){
+    const textField = document.getElementById("text-field");
+    textField.style.textAlign = "right";
+   
+});
+
+
+document.getElementById("btn-number").addEventListener("click", function(e){
+    const textField = document.getElementById("text-field");
+    const value = e.target.value;
+    textField.style.fontSize= value + "px";
+    
+   
+});
+
+document.getElementById("btn-text").addEventListener("click", function(){
+    const textField = document.getElementById("text-field");
+    textField.style.textTransform= "lowercase";
+   
+});
+//------------------------------------
+document.getElementById("btn-color").addEventListener("change",function(){
+    const textField = document.getElementById("text-field");
+    const colorValue = e.target.value;
+    textField.style.color = colorValue;
+})
+    
+document.getElementById("clear-text").addEventListener("click", function(){
+    const textField = document.getElementById("text-field");
+    textField.value = "";
+   
+});
